3cd999540c33b7eac35a99ac403c14b2fda4fd78
Cristi Tone <cristitone@192-168-0-167.rdsnet.ro>
2023-04-21 13:07:20 +0300

    Added loading indicator

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
+  useEffect(() => {
+    Router.events.on('routeChangeStart', NProgress.start);
+    Router.events.on('routeChangeComplete', NProgress.done);
+    Router.events.on('routeChangeError', NProgress.done);
+    return () => {
+      Router.events.off('routeChangeStart', NProgress.start);
+      Router.events.off('routeChangeComplete', NProgress.done);
+      Router.events.off('routeChangeError', NProgress.done);
+    };
+  }, []);
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
